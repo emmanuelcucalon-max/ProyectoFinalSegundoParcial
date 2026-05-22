5a13602c4133f21b146586aa5d94ccb4f8666348
Jamesjsfo <jsfernandez@uamv.edu.ni>
2026-05-22 12:39:30 -0600

    james

--- Program.cs
+++ Program.cs
@@ -20,6 +20,74 @@ Definir variables y arreglos.*/
 /*Integrante 2 — Entrada de datos
 Programar el ingreso de información.
 Validar datos.*/
+//Usar arreglos para almacenar:
+//Nombre del producto
+//Precio
+//Cantidad vendida
+//Debe permitir registrar mínimo 10 productos.//
+
+        string[] dias = new string[7] { "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo" };
+        double[] ventas = new double[7];
+        try
+        {
+            for (int i = 0; i < dias.Length; i++)
+            {
+                Console.WriteLine("Hoy es el dia " + dias[i]);
+                Console.WriteLine("Vendio algo el dia de hoy? (s/n)");
+                string rsp = Console.ReadLine();
+                if (rsp.ToLower() == "s")
+                {
+                
+                Console.WriteLine("Ingrese el nombre del producto vendido: ");
+                string producto = Console.ReadLine();
+                Console.WriteLine("Ingrese el precio del producto: ");
+                double precio = double.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese la cantidad vendida: ");
+                double cantidad = double.Parse(Console.ReadLine());
+                Console.WriteLine("¿Desea registrar otra venta el dia de hoy? (s/n)");
+                string respuesta = Console.ReadLine();
+                ventas[i] += precio * cantidad;
+                while (respuesta.ToLower() == "s")
+                {
+                    Console.WriteLine("Ingrese el nombre del producto vendido: ");
+                    producto = Console.ReadLine();
+                    Console.WriteLine("Ingrese el precio del producto: ");
+                    precio = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Ingrese la cantidad vendida: ");
+                    cantidad = double.Parse(Console.ReadLine());
+                    ventas[i] += precio * cantidad;
+                    Console.WriteLine("¿Desea registrar otra venta el dia de hoy? (s/n)");
+                    respuesta = Console.ReadLine();
+                }
+                }
+                else
+                {
+                    Console.WriteLine("No se registraron ventas el dia " + dias[i]);
+                }
+
+            }
+
+            double totalSemana = 0;
+            for (int i = 0; i < ventas.Length; i++)
+                totalSemana += ventas[i];
+            Console.WriteLine($"Total de ventas de la semana: {totalSemana:0.00}");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Error: Entrada no válida. Por favor, ingrese un número.");
+        }
+        
+    
+
+   
+    
+
+
+
+
+
+
+
 
 /*Integrante 3 — Procesamiento
 Realizar cálculos y estadísticas.*/
