aa41c1988a07a0af0164c81b99ce0215ec851f1c
emmanuelcucalon-max <ercucalon@uamv.edu.ni>
2026-05-22 11:33:55 -0600

    feat: instrucciones generales

--- Program.cs
+++ Program.cs
@@ -1,1 +1,30 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*Ejercicio: Sistema de Registro de Ventas usando Arreglos
+
+Contexto
+
+Una tienda de tecnología desea registrar las ventas realizadas durante una semana.
+El sistema será desarrollado en equipos de 4 estudiantes utilizando arreglos en C#.
+
+Objetivo del ejercicio
+
+Desarrollar un programa que permita almacenar y procesar información de ventas utilizando arreglos unidimensionales y bidimensionales.
+
+Integrantes del equipo
+
+Cada integrante tendrá un rol específico:*/
+
+/*Integrante 1 — Diseño de datos
+Crear los arreglos.
+Definir variables y arreglos.*/
+
+/*Integrante 2 — Entrada de datos
+Programar el ingreso de información.
+Validar datos.*/
+
+/*Integrante 3 — Procesamiento
+Realizar cálculos y estadísticas.*/
+
+/*Integrante 4 — Salida y pruebas
+Mostrar reportes.
+Probar el sistema y corregir errores.*/
+
