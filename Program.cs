1212002481fa3f2eab070df03ff6847c530d4b6c
emmanuelcucalon-max <ercucalon@uamv.edu.ni>
2026-05-22 12:24:21 -0600

    feat: Mi parte

--- Program.cs
+++ Program.cs
@@ -22,10 +22,44 @@ double[] PrecioProducto = new double[10];
 int[] CantidadProducto = new int[10];
 int[,] registroVentas = new int [10, 3];
 int TotalVendido = 0;
+int opcion= 0;
 
 Console.WriteLine("Bienvenido al sistema de registro de ventas");
 
 Console.WriteLine("==========Menú==========\n 1. Registrar productos\n 2. Mostrar productos\n 3. Calcular total vendido\n 4. Mostrar producto más vendido\n 5. Mostrar promedio de ventas\n 6. Salir");
+
+try
+{
+    Console.WriteLine("Introdizca la opcion que desea realizar");
+    opcion=int.Parse(Console.ReadLine()!);
+}
+catch (Exception ex)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine($"Ocurrió un error: {ex.Message}");
+    Console.ResetColor();
+}
+switch (opcion)
+{
+    case 1:
+        // Código para registrar productos
+        break;
+    case 2:
+        // Código para mostrar productos
+        break;
+    case 3:
+        // Código para calcular total vendido
+        break;
+    case 4:
+        // Código para mostrar producto más vendido
+        break;
+    case 5:
+        // Código para mostrar promedio de ventas
+        break;
+    case 6:
+        Console.WriteLine("Saliendo del programa...");
+        break;
+}
 /*Integrante 2 — Entrada de datos
 Programar el ingreso de información.
 Validar datos.*/
